7413f9fd8ae10e045f398c9764de4d36cff1f4a0
LeeVaughn <dlvprogramming@gmail.com>
2021-09-21 13:48:10 -0700

    Add New Course button

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -14,6 +14,13 @@ export default class Courses extends Component {
             <h2 className="course--label">Course</h2>
             <h3 className="course--title">Learn How to Program</h3>
           </Link>
+          <Link className="course--module course--add--module" to="/courses/create">
+            <span className="course--add--title">
+              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+              viewBox="0 0 13 13" class="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+              New Course
+            </span>
+          </Link>
         </div>
       </main>
     );
