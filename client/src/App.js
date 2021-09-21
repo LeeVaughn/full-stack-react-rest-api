fb4140d0f2dc095944f2fb5dd3bd262428cc4bfb
LeeVaughn <dlvprogramming@gmail.com>
2021-09-21 09:09:20 -0700

    Remove unnecessary div element

--- client/src/App.js
+++ client/src/App.js
@@ -24,18 +24,16 @@ const UserSignOutWithContext = withContext(UserSignOut);
 
 export default () => (
   <Router>
-    <div>
-      <HeaderWithContext />
+    <HeaderWithContext />
 
-      <Switch>
-        <Route exact path="/" component={Public} />
-        <PrivateRoute path="/authenticated" component={AuthWithContext} />
-        <PrivateRoute path="/settings" component={AuthWithContext} />
-        <Route path="/signin" component={UserSignInWithContext} />
-        <Route path="/signup" component={UserSignUpWithContext} />
-        <Route path="/signout" component={UserSignOutWithContext} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Public} />
+      <PrivateRoute path="/authenticated" component={AuthWithContext} />
+      <PrivateRoute path="/settings" component={AuthWithContext} />
+      <Route path="/signin" component={UserSignInWithContext} />
+      <Route path="/signup" component={UserSignUpWithContext} />
+      <Route path="/signout" component={UserSignOutWithContext} />
+      <Route component={NotFound} />
+    </Switch>
   </Router>
 );
