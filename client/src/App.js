b7807257cf3d60255e995fd36d43a2c577226353
LeeVaughn <dlvprogramming@gmail.com>
2021-09-28 14:50:15 -0700

    Add /courses/:id route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import Header from './components/Header';
 import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
 import NotFound from './components/NotFound';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
@@ -16,6 +17,7 @@ import withContext from "./Context";
 import PrivateRoute from "./PrivateRoute";
 
 const CoursesWithContext = withContext(Courses);
+const CourseDetailWithContext = withContext(CourseDetail);
 const HeaderWithContext = withContext(Header);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
@@ -27,6 +29,7 @@ export default () => (
 
     <Switch>
       <Route exact path="/" component={CoursesWithContext} />
+      <Route exact path="/courses/:id" component={CourseDetailWithContext} />
       <Route path="/signin" component={UserSignInWithContext} />
       <Route path="/signup" component={UserSignUpWithContext} />
       <Route path="/signout" component={UserSignOutWithContext} />
