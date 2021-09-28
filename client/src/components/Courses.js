69e5d54f0d76288499c36862231c42ec72bc0b4f
LeeVaughn <dlvprogramming@gmail.com>
2021-09-28 10:45:37 -0700

    Add functionality to display courses

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -9,29 +9,30 @@ export default class Courses extends Component {
   componentDidMount() {
     const { context } = this.props;
 
-    context.data
-      .getCourses()
+    context.data.getCourses()
       .then(data => {
-        if (data) {
-          this.setState({ courses: data });
-          console.log(this.state.courses)
-        }
+        this.setState({ courses: data });
+      })
+      .catch(err => {
+        console.log(err);
+        this.props.history.push(from);
       })
   }
 
   render() {
+    const { courses } = this.state;
+    console.log(courses)
+
     return (
       <main>
         <div className="wrap main--grid">
-          <Link className="course--module course--link" to="/notfound">
-            <h2 className="course--label">Course</h2>
-            <h3 className="course--title">Build a Basic Bookcase</h3>
-          </Link>
-          <Link className="course--module course--link" to="/notfound">
-            <h2 className="course--label">Course</h2>
-            <h3 className="course--title">Learn How to Program</h3>
-          </Link>
-          <Link className="course--module course--add--module" to="/courses/create">
+          {courses.map((course) => (
+            <Link className="course--module course--link" to={`/courses/${course.id}`} key={course.id}>
+              <h2 className="course--label">Course</h2>
+              <h3 className="course--title">{course.title}</h3>
+            </Link>
+          ))}
+          <Link className="course--module course--add--module" to={"/courses/create"}>
             <span className="course--add--title">
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
               viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
