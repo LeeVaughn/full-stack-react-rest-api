be9ac9fa4210b28f489ced0cbb2cdc9f35a9b11c
LeeVaughn <dlvprogramming@gmail.com>
2021-10-03 15:35:20 -0700

    Create new component

---
+++ client/src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <main>
+    <div className="wrap">
+      <h2>Error</h2>
+      <p>Sorry! An unexpected error has occurred.</p>
+    </div>
+  </main>
+);
