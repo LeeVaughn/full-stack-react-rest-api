424b486f6837b179dc5a1d5acfa41f2f659f4089
LeeVaughn <dlvprogramming@gmail.com>
2021-10-05 12:30:00 -0700

    Import Forbidden component and update routing

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,8 @@ import CourseDetail from "./components/CourseDetail";
 import UserSignIn from "./components/UserSignIn";
 import UserSignUp from "./components/UserSignUp";
 import UserSignOut from "./components/UserSignOut";
-import Error from "./components/Error";
+import UnhandledError from "./components/UnhandledError";
+import Forbidden from "./components/Forbidden";
 import NotFound from "./components/NotFound";
 
 
@@ -38,7 +39,9 @@ export default () => (
       <Route path="/signin" component={UserSignInWithContext} />
       <Route path="/signup" component={UserSignUpWithContext} />
       <Route path="/signout" component={UserSignOutWithContext} />
-      <Route path="/error" component={Error} />
+      <Route path="/error" component={UnhandledError} />
+      <Route path="/forbidden" component={Forbidden} />
+      <Route path="/notfound" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   </Router>
