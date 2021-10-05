ee0f0478428ad8f30792ff72f327ea5327181f2d
LeeVaughn <dlvprogramming@gmail.com>
2021-10-05 12:14:34 -0700

    import Error component and add to routing

--- client/src/App.js
+++ client/src/App.js
@@ -9,20 +9,22 @@ import Header from "./components/Header";
 import Courses from "./components/Courses";
 import UpdateCourse from "./components/UpdateCourse";
 import CourseDetail from "./components/CourseDetail";
-import NotFound from "./components/NotFound";
-import UserSignUp from "./components/UserSignUp";
 import UserSignIn from "./components/UserSignIn";
+import UserSignUp from "./components/UserSignUp";
 import UserSignOut from "./components/UserSignOut";
+import Error from "./components/Error";
+import NotFound from "./components/NotFound";
+
 
 import withContext from "./Context";
 import PrivateRoute from "./PrivateRoute";
 
+const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
 const UpdateCourseWithContext = withContext(UpdateCourse);
 const CourseDetailWithContext = withContext(CourseDetail);
-const HeaderWithContext = withContext(Header);
-const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignOutWithContext = withContext(UserSignOut);
 
 export default () => (
@@ -36,6 +38,7 @@ export default () => (
       <Route path="/signin" component={UserSignInWithContext} />
       <Route path="/signup" component={UserSignUpWithContext} />
       <Route path="/signout" component={UserSignOutWithContext} />
+      <Route path="/error" component={Error} />
       <Route component={NotFound} />
     </Switch>
   </Router>
